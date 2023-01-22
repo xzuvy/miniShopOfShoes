8d98407e83ea02f6fdf31e6dd2d6d7725ff89ac3
Pete <xzuvy27@gmail.com>
2023-01-22 15:31:16 +0300

    111
    dssd

--- miniShopOfShoes/ContentView.swift
+++ miniShopOfShoes/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var shoesIndex = 0
+    @State var namesOfBrand = ["Nike", "Puma", "Adidas"]
+    @State var imagesOfShoes = ["nike", "puma", "adidas"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack{
+            
+            Text("Название кроссовок - \(namesOfBrand[shoesIndex])")
+            Spacer()
+            ZStack{
+                
+                RoundedRectangle(cornerRadius: 15)
+                Image(imagesOfShoes[shoesIndex])
+                    .resizable()
+                    .frame(width: 300, height: 300)
+                
+                
+            }
+            .foregroundColor(Color.white)
+            .padding()
+            .animation(.spring(response: 0.7, blendDuration: 0.5))
+            
+            Picker(selection: Binding(get: {
+                self.shoesIndex
+            }, set: {
+                newValue in
+                self.shoesIndex = newValue
+            }),  label: Text("")){
+                ForEach(0..<namesOfBrand.count){
+                    Text(self.namesOfBrand[$0]).tag($0)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            Spacer().frame(height: 20)
+            .background(Color.red)
+            
         }
-        .padding()
+        
     }
 }
 
