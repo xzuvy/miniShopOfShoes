c143a0573c5cfd99ef4c897ee174fd9e356374c7
Pete <xzuvy27@gmail.com>
2023-01-22 15:02:27 +0300

    Initial Commit

---
+++ miniShopOfShoes/miniShopOfShoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniShopOfShoesApp.swift
+//  miniShopOfShoes
+//
+//  Created by Peter Clarke on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct miniShopOfShoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
